fab9ff625090d72230e24b852e602f47edfa28a3
Dippy2003 <sahanwijesi@gmail.com>
2026-08-26 10:08:16 +0530

    feat: extend script detection to devanagari, arabic and han

--- src/scripts.py
+++ src/scripts.py
@@ -5,6 +5,12 @@ def script_of(s: str) -> str:
             return "sinhala"
         if 0x0B80 <= cp <= 0x0BFF:
             return "tamil"
+        if 0x0900 <= cp <= 0x097F:
+            return "devanagari"
+        if 0x0600 <= cp <= 0x06FF:
+            return "arabic"
+        if 0x4E00 <= cp <= 0x9FFF:
+            return "han"
         if 0x0041 <= cp <= 0x007A:
             return "latin"
     return "other"
